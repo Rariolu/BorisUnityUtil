d09e7c2c75016a34e1e023a90e5a10da3e2e9614
Rariolu <rcoyneyr82011@gmail.com>
2020-02-23 03:55:13 +0000

    Added ResourceManager

---
+++ UbiUnity/ResourceManagement/RescItem.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace UbiUnity.ResourceManagement
+{
+    /// <summary>
+    /// A class which specifies a particular resource
+    /// and gives it a designated name.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    [Serializable]
+    public class RescItem<T>
+    {
+        [SerializeField]
+        public string Name;
+        [SerializeField]
+        public T Item;
+    }
+
+    /// <summary>
+    /// A struct used to identify specific instances of "RescItem"
+    /// inside the ResourceManager.
+    /// </summary>
+    struct RescItemKey
+    {
+        string strKey;
+        Type type;
+        public RescItemKey(string key, Type t)
+        {
+            strKey = key;
+            type = t;
+        }
+    }
+}
