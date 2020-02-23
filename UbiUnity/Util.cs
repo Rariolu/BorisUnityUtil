5c43fc8e2804d282003a2430dc59f66177a1b79f
Rariolu <rcoyneyr82011@gmail.com>
2020-02-23 01:45:28 +0000

    Added string normalisation, enum parsing, and enum array creation.

--- UbiUnity/Util.cs
+++ UbiUnity/Util.cs
@@ -9,6 +9,47 @@ namespace UbiUnity
     public static class Util
     {
         /// <summary>
+        /// Attempts to parse a given string to find an enum value of a given type.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="text"></param>
+        /// <param name="value">Either a valid match value or the default value of T.</param>
+        /// <returns>True if a valid match is found, false if not</returns>
+        public static bool EnumTryParse<T>(this string text, out T value) where T : struct
+        {
+            value = default(T);
+            Type type = typeof(T);
+            if (!type.IsEnum || text == null)
+            {
+                return false;
+            }
+            foreach (T eVal in GetEnumValues<T>())
+            {
+                if (eVal.NormaliseString() == text.NormaliseString())
+                {
+                    value = eVal;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Return an array of the given enum type.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static T[] GetEnumValues<T>() where T : struct
+        {
+            Type type = typeof(T);
+            if (!type.IsEnum)
+            {
+                return new T[0];
+            }
+            return Enum.GetValues(type).Cast<T>().ToArray();
+        }
+
+        /// <summary>
         /// Close the game: stops the playthrough if
         /// running in the editor and stops the executable
         /// file if the build is running.
@@ -25,6 +66,16 @@ namespace UbiUnity
             //Close the executable.
             Application.Quit();
 #endif
+        }
+
+        /// <summary>
+        /// Trims text and sets it to a uniform case for easy comparison.
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public static string NormaliseString(this object obj)
+        {
+            return obj.ToString().Trim().ToLower();
         }
     }
 }
